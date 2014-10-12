c8bf991c11f0f7f1dff8de5e3b911a5f08e5401e
Chee-Hong Hsia <Chee-Hong Hsia@LAPTOP-CHSIA>
2014-10-12 22:07:22 +0200

    Created a facade UserService class

---
+++ MyWebProject/src/main/java/com/cheehong/service/UserService.java
@@ -0,0 +1,37 @@
+package com.cheehong.service;
+
+import java.util.List;
+
+import com.cheehong.domain.User;
+
+public interface UserService {
+
+	/**
+	 * Method for creating a User.
+	 */
+	public void create();
+
+	/**
+	 * Method for modifying a specific user.
+	 */
+	public void modify(long userID);
+
+	/**
+	 * Method for deleting a specific user.
+	 */
+	public void delete(long userID);
+
+	/**
+	 * Method for retrieving all users.
+	 * @return a list of users.
+	 */
+	public List<User> getAllUsers();
+
+	/**
+	 * Method for retrieving a specific user.
+	 * @param userID
+	 * @return a user.
+	 */
+	public User getUser(long userID);
+
+}
