b95f4d4aa6a8b7a326e73ca0eb2490f258019513
Chee-Hong Hsia <Chee-Hong Hsia@LAPTOP-CHSIA>
2014-10-12 21:40:41 +0200

    Removed RootController class

--- MyWebProject/src/main/java/com/cheehong/controller/UserController.java
+++ MyWebProject/src/main/java/com/cheehong/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cheehong.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,15 @@ import com.cheehong.domain.User;
 
 @Controller
 public class UserController {
+	
+	@RequestMapping(value = { "/", "index.html", "index.jsp" }, method = RequestMethod.GET)
+    public String getCreateUserPage(ModelMap model) {
+		model.addAttribute("createUserForm", new User());
+        return "createUser";
+    }
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String processSubmit(@ModelAttribute("createUserForm") User user, BindingResult result, SessionStatus status) {
-		
-		
+	public String createUser(@ModelAttribute("createUserForm") User user, BindingResult result, SessionStatus status) {
 		return "userOverview";
 	}
 
